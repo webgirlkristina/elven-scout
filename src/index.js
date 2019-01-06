81697a55b3f6077afde6a43faba5454076214dfd
webgirlkristina <webgirlkristina@gmail.com>
2019-01-07 02:29:57 +0300

    Lesson 1

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import { Game } from "./game";
+
+console.log("И сейчас работает");
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  mode: "development",
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/elven-scout.js'
+  },
+  devServer: {
+    contentBase: './dist'
+  },  
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+};
